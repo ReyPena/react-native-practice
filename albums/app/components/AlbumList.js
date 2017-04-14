d7e5b16d9e94d3aa672439485e748be0b21a5eaa
ReyPena <zonickblack@gmail.com>
2017-04-14 16:11:49 -0600

    Add axios to the project

--- albums/app/components/AlbumList.js
+++ albums/app/components/AlbumList.js
@@ -1,12 +1,31 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Albun List</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums/')
+      .then((response) => {
+        this.setState({ albums: response.data });
+      });
+  }
+
+  render() {
+    let content = null;
+    console.log(this.state);
+    if (this.state.albums.length === 0) {
+      content = 'Loading';
+    } else {
+      content = 'hello';
+    }
+    return (
+      <View>
+        <Text>{ content }</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;

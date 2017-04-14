3179bfc5fc67e36371a17ec43f8d76973a78e736
ReyPena <zonickblack@gmail.com>
2017-04-14 10:59:27 -0600

    Project setup and set up for share code between devices.

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,6 +1,5 @@
 // Index.android.js - This is the Main file for android device
 import { AppRegistry } from 'react-native';
-import React from 'react';
 import App from './app/index';
 
 AppRegistry.registerComponent('albums', () => App);
--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,1 +1,6 @@
 // Index.ios.js - This is the Main file for IOS device
+// Index.android.js - This is the Main file for android device
+import { AppRegistry } from 'react-native';
+import App from './app/index';
+
+AppRegistry.registerComponent('albums', () => App);
